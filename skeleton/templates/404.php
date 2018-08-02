c2b9c53eafe25d3aeb4c26c12269a98c0bd7513d
Åsmund Stavdahl <smund3@gmail.com>
2018-08-02 23:47:50 +0200

    small tweaks to 404 template

--- skeleton/templates/404.php
+++ skeleton/templates/404.php
@@ -1,6 +1,4 @@
-<p>
-	<pre><?= $_SERVER["REMOTE_ADDR"] ?> used <?= $_SERVER["REQUEST_URI"] ?>!
+<pre><?= $_SERVER["REMOTE_ADDR"] ?> used <?= $_SERVER["REQUEST_URI"] ?>!
 <?= $_SERVER["REMOTE_ADDR"] ?>'s attack missed!
 Enemy <?= $_SERVER["SERVER_NAME"] ?> used 404 Not Found!
 <?= $_SERVER["REMOTE_ADDR"] ?> became confused!</pre>
-</p>
