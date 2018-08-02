94b09cf85da51fe548d058a3a2419471c996717c
Åsmund Stavdahl <smund3@gmail.com>
2018-08-02 23:47:24 +0200

    improve index.php and its docs

--- skeleton/public/index.php
+++ skeleton/public/index.php
@@ -11,6 +11,7 @@ use \Rapd\Router;
 use \Rapd\View;
 use \Rapd\Environment;
 
+# Configure a function to be used by View::render()
 View::setRenderer(function(string $template, array $data = []){
 	require_once "../src/template-preparations.php";
 	extract(Environment::getAll());
@@ -20,11 +21,21 @@ View::setRenderer(function(string $template, array $data = []){
 	include "../templates/footer.php";
 });
 
-Router::setBaseURL("/");
+# The router's application base path defaults to "/".
+#Router::setApplicationBasePath("/myproject");
+Router::setApplicationBasePath(str_replace($_SERVER["DOCUMENT_ROOT"], "", __DIR__));
+# Include PHP files from the directory
 Router::loadDirectory("../routes");
 
+# Set some environment variables used by the header template
+Environment::set("TITLE", "rapd/skeleton");
+Environment::set("AUTHOR", "Åsmund Stavdahl");
+Environment::set("DESCRIPTION", "Default description of the rapd/skeleton");
+
+# Generate a response based on the request and the configured routes
 $result = Router::run();
 
+# Send the response
 if($result === false){
 	header('HTTP/1.1 404 Not Found');
 	echo View::render("404");
