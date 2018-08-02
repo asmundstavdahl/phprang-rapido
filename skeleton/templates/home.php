f1f1cbf0000039dada2211fd522aa4e3cef6745c
Åsmund Stavdahl <smund3@gmail.com>
2018-08-02 23:50:53 +0200

    remove preceeding spaces from home template's li-s

--- skeleton/templates/home.php
+++ skeleton/templates/home.php
@@ -2,25 +2,9 @@
 <p>
 	Try one of these:
 	<ul>
-		<li>
-			<a href="<?= route("just_hello") ?>">
-				Just hello.
-			</a>
-		</li>
-		<li>
-			<a href="<?= route("hello_world") ?>">
-				Hello world!
-			</a>
-		</li>
-		<li>
-			<a href="<?= route("hello_something", ["person"]) ?>">
-				Hello person!
-			</a>
-		</li>
-		<li>
-			<a href="/unhandled/route">
-				Hello 404!
-			</a>
-		</li>
+		<li><a href="<?= route("just_hello") ?>">Just hello.</a></li>
+		<li><a href="<?= route("hello_world") ?>">Hello world!</a></li>
+		<li><a href="<?= route("hello_something", ["person"]) ?>">Hello person!</a></li>
+		<li><a href="unhandled/route">Hello 404!</a></li>
 	</ul>
 </p>
