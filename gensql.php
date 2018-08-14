bbf2e03b2bb0b7d4573602be62a1813f8f6c4e7e
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2018-08-14 16:02:26 +0200

    add gensql.php

---
+++ gensql.php
@@ -0,0 +1,33 @@
+<?php
+
+require "src/autoload.php";
+if(file_exists("vendor/autoload.php")){
+	include "vendor/autoload.php";
+} else {
+	echo "W: No vendor autoload file.\n";
+}
+
+$files = explode(
+	"\n", 
+	trim(`grep -RFl '\$fields' src/`)
+);
+
+$types = [
+	integer::class => "INTEGER NOT NULL DEFAULT 0",
+	string::class => "TEXT NOT NULL DEFAULT ''",
+	float::class => "REAL NOT NULL DEFAULT 0.0",
+];
+
+foreach($files as $file):
+	$className = preg_replace('`src/|\.php`', "", $file);
+	$prefix = "  ";
+?>
+
+CREATE TABLE IF NOT EXISTS <?= $className::getTable() ?> (
+<?php foreach($className::$fields as $field => $type): ?>
+	<?= $prefix. $field ?> <?= $types[$type] ?>
+
+<?php $prefix = ", "; ?>
+<?php endforeach; ?>
+)
+<?php endforeach; ?>
